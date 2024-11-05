33e94ca3eefd775f822c2498d453768a3c87c88c
xg <1192736652@qq.com>
2024-11-05 16:22:44 +0800

    提交

---
+++ rpc-core/src/main/java/com/xg/rpc/bootstrap/ConsumerBootstrap.java
@@ -0,0 +1,20 @@
+package com.xg.rpc.bootstrap;
+
+import com.xg.rpc.RpcApplication;
+
+/**
+ * 服务消费者启动类（初始化）
+ *
+
+ */
+public class ConsumerBootstrap {
+
+    /**
+     * 初始化
+     */
+    public static void init() {
+        // RPC 框架初始化（配置和注册中心）
+        RpcApplication.init();
+    }
+
+}
---
+++ rpc-core/src/main/java/com/xg/rpc/config/RegistryConfig.java
@@ -0,0 +1,38 @@
+package com.xg.rpc.config;
+
+import com.xg.rpc.registry.RegistryKeys;
+import lombok.Data;
+
+/**
+ * RPC 框架注册中心配置
+ *
+学习圈</a>
+ */
+@Data
+public class RegistryConfig {
+
+    /**
+     * 注册中心类别
+     */
+    private String registry = RegistryKeys.ETCD;
+
+    /**
+     * 注册中心地址
+     */
+    private String address = "http://localhost:2380";
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 超时时间（单位毫秒）
+     */
+    private Long timeout = 10000L;
+}
